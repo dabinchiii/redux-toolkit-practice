0f32efcfeff37b8c892ba3fa3be52d295007f9bc
dabinchiii <dabinchi388@naver.com>
2023-07-12 02:15:31 +0900

    feat: Add reducer

--- REDUX-DEMO/index.js
+++ REDUX-DEMO/index.js
@@ -5,6 +5,27 @@ function orderCake() {
     // action 정의 (action: type 프로퍼티를 가지는 객체)
     return {
         type: CAKE_ORDERED,
-        quantity: 1
+        quantity: 1,
+    }
+}
+
+// 초기 상태 정의 (object 형태)
+const initialState = {
+    numOfCakes: 10,
+    anotherProperty: 0,
+}
+
+// reducer
+// arguments : previousState, action
+// return value : newState
+const reducer = (state = initialState, action) => {
+    switch(action.type) { // switch문으로 action type에 따라 분기
+        case CAKE_ORDERED:
+            return {
+                ...state, // state object 복사
+                numOfCakes: state.numOfCakes - 1, // 변경 필요한 프로퍼티만 업데이트
+            };
+        default: // 이전 상태 그대로 반환
+            return state;
     }
 }
