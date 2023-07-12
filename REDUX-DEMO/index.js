9aefb5ecc11e865ae752af8dc582ff4758938fe3
dabinchiii <dabinchi388@naver.com>
2023-07-12 15:10:57 +0900

    feat: Add action - restockCake

--- REDUX-DEMO/index.js
+++ REDUX-DEMO/index.js
@@ -1,15 +1,24 @@
 const redux = require('redux');
 const createStore = redux.createStore;
 
-const CAKE_ORDERED = 'CAKE_ORDERED'; // 스펠링 실수 방지를 위해 상수로 선언
+// 스펠링 실수 방지를 위해 상수로 선언
+const CAKE_ORDERED = 'CAKE_ORDERED';
+const CAKE_RESTOCKED = 'CAKE_RESTOCKED';
 
 // action creator : a function that returns an object
 function orderCake() {
     // action 정의 (action: type 프로퍼티를 가지는 객체)
     return {
         type: CAKE_ORDERED,
-        quantity: 1,
+        payload: 1, // payload : 전송할 추가 정보를 위한 프로퍼티 (redux convention)
     }
+}
+
+function restockCake(qty = 1) {
+    return {
+        type: CAKE_RESTOCKED,
+        payload: qty,
+    };
 }
 
 // 초기 상태 정의 (object 형태)
@@ -27,6 +36,11 @@ const reducer = (state = initialState, action) => {
                 ...state, // state object 복사
                 numOfCakes: state.numOfCakes - 1, // 변경 필요한 프로퍼티만 업데이트
             };
+        case CAKE_RESTOCKED:
+            return {
+                ...state,
+                numOfCakes: state.numOfCakes + action.payload,
+            }
         default: // 이전 상태 그대로 반환
             return state;
     }
@@ -48,6 +62,7 @@ const unsubscribe = store.subscribe(() =>
 store.dispatch(orderCake()); // action creator가 action 반환
 store.dispatch(orderCake());
 store.dispatch(orderCake());
+store.dispatch(restockCake(3));
 
 // listener 등록 취소
 unsubscribe();
