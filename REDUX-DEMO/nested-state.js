af714651543e1df0bb2a5aa9fe4f475d8992c62a
dabinchiii <dabinchi388@naver.com>
2023-07-12 16:28:05 +0900

    feat: Add store for address

---
+++ REDUX-DEMO/nested-state.js
@@ -0,0 +1,50 @@
+const redux = require('redux');
+
+const initialState = {
+    name: 'Vishwas',
+    address: { // object state
+        street: '123 Main St',
+        city: 'Boston',
+        state: 'MA',
+    },
+};
+
+const STREET_UPDATED = 'STREET_UPDATED'; // action type
+
+// action creator
+const updateStreet = (street) => {
+    return {
+        type: STREET_UPDATED,
+        payload: street,
+    };
+};
+
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case STREET_UPDATED:
+            return {
+                ...state,
+                address: {
+                    ...state.address,
+                    street: action.payload,
+                },
+            }
+        default:
+            return state; 
+    }
+}
+
+// store 생성
+const store = redux.createStore(reducer);
+console.log('Initial State', initialState);
+
+// listener 등록
+const unsubscribe = store.subscribe(() => {
+    console.log('Updated State', store.getState());
+});
+
+// dispatch로 state 변경
+store.dispatch(updateStreet('456 Main St'));
+
+// listener 등록 취소
+unsubscribe();
